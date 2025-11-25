7b5f271f94a0418eccfcd4303a7eac043ada1509
gordon-coder <gordonpeng27@gmail.com>
2025-11-25 20:54:18 +0800

    added sports_day to urls

--- APIs/urls.py
+++ APIs/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from sports_day import urls as sports_day_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sports_day/', include(sports_day_urls)),
 ]
---
+++ sports_day/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from sports_day.views import *
+
+urlpatterns = [
+
+]
